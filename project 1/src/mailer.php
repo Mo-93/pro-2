ac6a48ea5761d630c6350c05b7adcc692cb67f69
Mo-93 <Mohamad-356125@hotmail.com>
2023-10-01 12:12:44 +0200

    add phpmailer en composer

--- project 1/src/mailer.php
+++ project 1/src/mailer.php
@@ -28,38 +28,38 @@ use Monolog\Handler\StreamHandler;
 //Load Composer's autoloader
 require '../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable( DIR . '/');
-$dotenv->load();
-
 //Create an instance; passing true enables exceptions
 $mail = new PHPMailer(true);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    $firstname = htmlspecialchars($_POST["firstname"]);
-    $Email = htmlspecialchars($_POST["Email"]);
-    $klacht = htmlspecialchars($_POST["klacht"]);
+    
+    $voornaam = htmlspecialchars($_POST["voornaam"]);
+    $achternaam = htmlspecialchars($_POST["achternaam"]);
+    $email = htmlspecialchars($_POST["email"]);
+
+    
+   
+
 
 }
     
 
 try {
     //Server settings
-
+   
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = $_ENV['SMTP_HOST'] ;                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = $_ENV['SMTP_USERNAME'];                     //SMTP username
     $mail->Password   = $_ENV['SMTP_PASSWORD'];                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = $_ENV['SMTP_PORT'];
-
-                                    //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
+    $mail->Port       = $_ENV['SMTP_PORT'];                               //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
 
     //Recipients
     $mail->setFrom('moh301530@gmail.com', 'Mohamad');
-    $mail->addAddress($Email, $firstname);     
-    $mail->addAddress($Email, $firstname);     
+    $mail->addAddress($email,$voornaam,$achternaam);     
+    $mail->addAddress($email,$voornaam,$achternaam);     
     $mail->addCC('moh301530@gmail.com');
 
 
@@ -67,8 +67,8 @@ try {
 
    //Content
    $mail->isHTML(true);                                  //Set email format to HTML
-   $mail->Subject = 'uw klacht is in behandeling, uw behandeling nummer is  ';
-   $mail->Body    = 'Dit is uw klacht: ' . $klacht ;
+   $mail->Subject = 'welcom in ons group ';
+   $mail->Body    = 'nieuw student ';
    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
    $mail->send();
